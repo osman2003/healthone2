7c8765ce9d0202a2b53e5c5272f378446bd9ef72
Osman <302549463@student.rocmondriaan.nl>
2020-12-10 14:16:58 +0100

    commit 1

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,18 @@
+<?php
+    namespace App\Controller;
+
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation\route;
+
+
+    class AdminController extends AbstractController
+    {
+        /**
+         * @Route("/" , name="homepage")
+         */
+        public function  homepageAction(){
+            return new Response( "<h1>welkom</h1>");
+            // return $this->render('admin/homepage.html.twig');
+        }
+    }
